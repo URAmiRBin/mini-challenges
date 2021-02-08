04235527742b76da81ac080acb19f2aecdf48803
Amir Bin <ur.amirbin@gmail.com>
2021-02-08 16:44:25 +0330

    Sudoku docs and comments

--- soduko/sudoku.py
+++ soduko/sudoku.py
@@ -2,16 +2,29 @@ import numpy as np
 
 
 class Table:
+    """
+    This class represents a sudoku table
+
+    colors is a list of colors, colors with smaller index has more priority
+    content is the real table
+    size is the size of content (row or columns)
+    domains is valid answers for each cell, each answer is a cell stored in corresponding index
+    domains is -1 if that position of table is filled already and indicates that this cell should not be changed 
+    """
+
     def __init__(self, input_string, colors):
         self.colors = colors
         self.size = len(input_string)
         self.content = []
         self.domains = [[[] for x in range(self.size)] for y in range(self.size)]
+
+        # Fill content
         for i in range(self.size):
             self.content.append([])
             for j in range(self.size):
                 self.content[i].append(Cell(input_string[i][j][0], input_string[i][j][1]))
 
+        # Fill initial valid domains
         for i in range(self.size):
             for j in range(self.size):
                 if self.content[i][j].isFilled():
@@ -32,6 +45,9 @@ class Table:
         
     
     def mrv(self):
+        # Calculate number of remaining values for unfilled cells
+        # return index of next cell to work with
+
         min_index = [0, 0]
         min_val = 10000
         remaining_values = [[0 for x in range(self.size)] for y in range(self.size)]
@@ -42,15 +58,22 @@ class Table:
                 else:
                     remaining_values[i][j] = len(self.domains[i][j])
                 if remaining_values[i][j] < min_val:
-                    if remaining_values[i][j] == 0:
-                        return [-1, -1]
                     min_val = remaining_values[i][j]
                     min_index[0] = i
                     min_index[1] = j
         return min_index
 
     def updateDomains(self,x , y, cell):
+        # Update domains of valid candidates
+        # for effected cells according to the given cell
+        # return false if at least one cell has no domain left
+        # meaning this solution gets stuck
+
+        # Set the given cell domain -1
+        # it means that it's fixed and should not be changed
         self.domains[x][y] = -1
+
+        # Update domain values for this column
         for i in range(self.size):
             if self.domains[i][y] == -1:
                 continue
@@ -59,6 +82,7 @@ class Table:
                     if not self.isValid(i, y, domain):
                         self.domains[i][y].remove(domain)
         
+        # Update domain values for this row
         for j in range(self.size):
             if self.domains[x][j] == -1:
                 continue
@@ -67,6 +91,7 @@ class Table:
                     if not self.isValid(x, j, domain):
                         self.domains[x][j].remove(domain)
 
+        # Check if any cell has domain values to work with
         for i in range(self.size):
             for j in range(self.size):
                 if self.domains[i][j] == -1:
@@ -77,54 +102,64 @@ class Table:
         return True
     
     def solve(self):
+        # Solve the problem
+        # Return false if the puzzle is not solvable
+
+        # Get next table cell to set values in
         next = self.mrv()
         
+        # Check if minimum remaining value is filled
+        # in this case there is no "real" value remaining so the puzzle is solved
         if self.content[next[0]][next[1]].isFilled():
             return True
         
+        # Save a backup in the case of need
         original_cell = self.content[next[0]][next[1]]
         original_domains = self.domains
 
+        # Set all valid values and backtracks using recursion
         for domain in self.domains[next[0]][next[1]]:
-            if self.isValid(next[0], next[1], domain):
-                self.content[next[0]][next[1]] = domain
-                res = self.updateDomains(next[0], next[1], domain)
+            # Set valid domain
+            self.content[next[0]][next[1]] = domain
+            # Forward check to see if this change locks the problem or not
+            # Update domains according to this change
+            res = self.updateDomains(next[0], next[1], domain)
 
-                if self.solve() and res:
-                    return True
-                else:
-                    self.content[next[0]][next[1]] = original_cell
-                    self.domains = original_domains
+            # proceed to solve the problem
+            if self.solve() and res:
+                return True
+            # backtrack if not successful
+            else:
+                self.content[next[0]][next[1]] = original_cell
+                self.domains = original_domains
         
         return False        
 
-    def isSolved(self):
-        for row in self.content:
-            for cell in row:
-                if not cell.isFilled():
-                    return False
-        return True
-                
-                        
-
     def isValid(self, x, y, cell):
-        # print("Checking validity of ", cell.display(), " in ", x, " ", y)
+        # Check if a given cell is valid in place (x, y) in the table
+        # return true if it's valid
+
+        # Check columnwise uniqueness
         for i in range(self.size):
             if i == x:
                 continue
             if cell.number == self.content[i][y].number:
                 return False
-
+        
+        # Check rowwise uniqueness
         for j in range(self.size):
             if j == y:
                 continue
             if cell.number == self.content[x][j].number:
                 return False
 
+        # Check neighbors
         for n in self.getNeighbors(x, y):
+            # Check if neighbors have distinct colors
             if cell.color == self.content[n[0]][n[1]].color:
                 return False
             
+            # Check set color priority match the numbers
             if self.content[n[0]][n[1]].isFilled():
                 if self.colors.index(cell.color) < self.colors.index(self.content[n[0]][n[1]].color):
                     if cell.number < self.content[n[0]][n[1]].number:
@@ -140,6 +175,8 @@ class Table:
 
     
     def getNeighbors(self, x, y):
+        # Find neighbors of a given cell
+        # Note that diagonal adjacent cells are not considers neighbors
         neighbors = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]]
 
         for n in neighbors:
@@ -154,6 +191,13 @@ class Table:
 
 
 class Cell:
+    """
+    This class represents each cell of the sudoku table
+
+    number is a string representing the number of cell
+    color is a string representing the number of cell
+    """
+
     def __init__(self, number, color):
         self.number = number
         self.color = color
@@ -162,16 +206,19 @@ class Cell:
         return self.number + self.color
 
     def isFilled(self):
+        # return true if the cell has both number and color set
         if self.number != "*" and self.color != "#":
             return True
         return False
 
     def hasOnlyNumber(self):
+        # return true if color value is set to default and number is set
         if self.number != "*" and self.color == "#":
             return True
         return False
 
     def hasOnlyColor(self):
+        # return true if number value is set to default and color is set
         if self.number == "*" and self.color != "#":
             return True
         return False
@@ -187,9 +234,12 @@ def read_input():
 
     return colors, table
 
+# Read inputs
 colors, tableList = read_input()
+# Generate table using given input
 table = Table(tableList, colors)
 
+# Solve sudoku
 result = table.solve()
 if result:
     table.display()
